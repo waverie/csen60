47c1913e9eca9f81fe37921bb53a2b93dc87e766
waverie <acwade@scu.edu>
2024-10-21 11:32:06 -0700

    wrote some javascript

--- week5/index.js
+++ week5/index.js
@@ -1,1 +1,22 @@
-console.log("hello world")
+// console.log("hello world")
+// alert('Hello world!');
+window.document.write('<p>Welcome to CSEN 60!</p>');
+
+// document.append('<p>This  should come after the h1</p>')
+
+/* function to contribute the sum of numbers.
+    Inputs: a, b nujmbers to be added.
+    Output: returns the sum of a and b.
+*/
+// TODO fix this function
+
+/**
+ * @description this function adds two numbers
+ * @param {*} a
+ * @param {*} b
+ * @returns
+ */
+
+function add(a, b) {
+    return a + b;
+}
