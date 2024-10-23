eb6aa9d7d116ac340e427af9c54de5b2882e9881
waverie <acwade@scu.edu>
2024-10-23 11:34:41 -0700

    learned about variables and operators

--- week5/index.js
+++ week5/index.js
@@ -20,3 +20,49 @@ window.document.write('<p>Welcome to CSEN 60!</p>');
 function add(a, b) {
     return a + b;
 }
+
+const x = 100; // input variable
+if (true) {
+    x = 200; // same variable
+}
+
+console.log(x); // output 200
+
+let y
+y = null
+let z = 0
+console.log(y, z)
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date().getHours()}`;
+console.log(response)
+
+const age = 20;
+
+if (age > 18) {
+    console.log("You are of age");
+    
+} else {
+    console.log('You arent at age. Please wait $(18 - age) years');
+}
+let isOverAge = (age > 18);
+
+console.log("\n", isOverAge)
+
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};
+
+console.log(person["age"])
+
+console.log(typeof("hello world!"))
+
+alert("This is a important message");
+
+// Strict equality -- 1. are these the same type? 2. Are these the same value
+console.log(3 === '3'); // false
+
+// Loose equality
+console.log(3 == '3'); // true
