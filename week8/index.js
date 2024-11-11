7c538c0d722bae4b53492e6db1305ea0a42997d8
waverie <acwade@scu.edu>
2024-11-11 12:48:09 -0800

    Create new server

--- week8/index.js
+++ week8/index.js
@@ -1,9 +1,29 @@
-getData() {
-    const response = await fetch("https://uselessfacts.jsph.pl/");
-    console.log();
-    getData();
-    const data = await .json;
-    getData();
+
+// this code runs synchronously
+function showRandomFact() {
+    // lots of code
+    console.log("showData function finished")
 }
 
-getData().finished();
+// this code runs asynchronously
+async function getRandomFact() {
+    const response = fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+    // console.log(response)
+    const data = await response.json()
+    console.log(data.text, "\n")
+    console.log("getData finished")
+}
+
+async function getTodaysFact() {
+    const response = fetch("https://uselessfacts.jsph.pl/api/v2/facts/today")
+    // console.log(response)
+    const data = await response.json()
+    console.log(data.text, "\n")
+    console.log("getData finished")
+}
+
+// getRandomFact()
+getTodaysFact()
+
+// getData()
+// showData()
