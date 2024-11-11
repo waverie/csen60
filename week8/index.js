bbf6faba9e38af7558a37cf30d2338404d2fdbf9
waverie <acwade@scu.edu>
2024-11-11 11:34:28 -0800

    Initial Commit for Week 8

---
+++ week8/index.js
@@ -0,0 +1,9 @@
+getData() {
+    const response = await fetch("https://uselessfacts.jsph.pl/");
+    console.log();
+    getData();
+    const data = await .json;
+    getData();
+}
+
+getData().finished();
